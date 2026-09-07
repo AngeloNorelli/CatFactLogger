884be67ae9adbc0e9ab61bef48908a17021e9350
AngeloNorelli <angnorelli@gmail.com>
2026-09-07 23:38:50 +0200

    refactor the base url - now is in appsettings.json

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@ using Microsoft.Extensions.Hosting;
 using CatFactLogger.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
+var apiBaseUrl = builder.Configuration["CatFactApi:BaseUrl"] 
+  ?? throw new InvalidOperationException("CatFactApi:BaseUrl is not configured in appsettings.json.");
 
 builder.Services.AddHttpClient<ICatFactApiClient, CatFactApiClient>(client =>
 {
-  client.BaseAddress = new Uri("https://catfact.ninja/");
+  client.BaseAddress = new Uri(apiBaseUrl);
   client.Timeout = TimeSpan.FromSeconds(10);
 });
 
